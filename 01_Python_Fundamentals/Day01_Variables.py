8c890f473424544300f9179db02e2b75f5ed8979
milindresearch <lonaremilind@gmail.com>
2026-06-06 23:52:28 +0530

    Add Day 01 Variables and Data Types example

---
+++ 01_Python_Fundamentals/Day01_Variables.py
@@ -0,0 +1,9 @@
+# Day 01 - Variables and Data Types
+
+country = "India"
+gdp_trillion_usd = 3.9
+defence_budget_billion_usd = 81
+
+print("Country:", country)
+print("GDP (Trillion USD):", gdp_trillion_usd)
+print("Defence Budget (Billion USD):", defence_budget_billion_usd)
