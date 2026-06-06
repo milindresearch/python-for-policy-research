1eb2fa3ee8f2356465a184f5b89f87c8cb6e0196
milindresearch <lonaremilind@gmail.com>
2026-06-07 00:03:43 +0530

    Add examples of lists and dictionaries in Python

---
+++ 01_Python_Fundamentals/Day02_Lists_Dictionaries.py
@@ -0,0 +1,22 @@
+Day 02 - Lists and Dictionaries
+
+List Example
+
+major_defence_partners = [
+"United States",
+"France",
+"Russia",
+"Israel"
+]
+
+print(major_defence_partners)
+
+Dictionary Example
+
+india_defence_profile = {
+"country": "India",
+"service_branches": 3,
+"nuclear_power": True
+}
+
+print(india_defence_profile)
